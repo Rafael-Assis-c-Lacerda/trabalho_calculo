1ec488cb013e6bddce9b49c7a2eb0d82093d629c
Rafael Assis Carvalho Lacerda <rafaelassisclacerda@gmail.com>
2025-05-06 22:38:10 -0300

    Add files via upload

--- trabalho_calculo/main.c
+++ trabalho_calculo/main.c
@@ -2,30 +2,66 @@
 #include <stdlib.h>
 #include <math.h>
 
-//função que recebe os pontos e retorna salva o valor do coeficiente angular e o b
-void calcularReta(float x1,float y1,float x2,float y2,float* a,float* b){
-    *a = (y2-y1)/(x2-x1);
-    *b = (-*a*x1+y1);
+// função que recebe os pontos e retorna salva o valor do coeficiente angular e o b
+void calcularReta(double x1, double y1, double x2, double y2, double* a, double* b) {
+    *a = (y2 - y1) / (x2 - x1);
+    *b = (-*a * x1 + y1);
 }
 
-void calcularPontos(float x, float* ponto[2]){
-    *ponto[0]=x;
-    *ponto[1]= pow(x,2);//grau da equação
+void calcularPontos(double x, double ponto[2]) {
+    ponto[0] = x;
+    ponto[1] = x*x;  // ou outra função f(x)
 }
 
-void retaTangenteX2(float aproximacao){
-    float ponto[2];
-    while(1){
-       calcularPontos(0, &ponto);
+void retaSecante(double x1, double y1, double pontoInicial, char direcao) {
+    double ponto[2];
+    double pontoAproximando;
+
+    double a;
+    double b;
+
+    if (direcao == 'D'||direcao == 'd') {
+        pontoAproximando = 0.5;
+
+        // Primeira aproximação grosseira
+        for (int i = 0; i < 4; i++) {
+            calcularPontos((pontoInicial + pontoAproximando), ponto);
+            calcularReta(x1, y1, ponto[0], ponto[1], &a, &b);
+            printf("%f | %f | f(x) = %lf x + %lf\n",(pontoInicial + pontoAproximando),ponto[1], a, b);
+            pontoAproximando -= 0.1;
+        }
+
+        // Segunda aproximação refinada
+        for (int i = 1; i <= 10; i++) {
+            calcularPontos((pontoInicial + pontoAproximando), ponto);
+            calcularReta(x1, y1, ponto[0], ponto[1], &a, &b);
+            printf("%f | %f | f(x) = %lf x + %lf\n",(pontoInicial + pontoAproximando),ponto[1], a, b);
+            pontoAproximando = pontoAproximando/10;
+        }
+    } else if (direcao == 'E'||direcao == 'e') {
+        pontoAproximando = 0.5;
+
+        // Primeira aproximação grosseira
+        for (int i = 0; i < 4; i++) {
+            calcularPontos((pontoInicial - pontoAproximando), ponto);
+            calcularReta(x1, y1, ponto[0], ponto[1], &a, &b);
+            printf("%f | %f | f(x) = %lf x + %lf\n",(pontoInicial - pontoAproximando),ponto[1], a, b);
+            pontoAproximando += 0.1;
+        }
+
+        // Segunda aproximação refinada
+        for (int i = 0; i < 10; i++) {
+            calcularPontos((pontoInicial - pontoAproximando), ponto);
+            calcularReta(x1, y1, ponto[0], ponto[1], &a, &b);
+            printf("%f | %f | f(x) = %lf x + %lf\n",(pontoInicial - pontoAproximando),ponto[1], a, b);
+            pontoAproximando = pontoAproximando/10;
+        }
     }
 }
 
-int main()
-{
-    float a;
-    float b;
+int main() {
+    char direcao = 'D';
+    retaSecante(2, 4, 2, direcao);
 
-    calcularReta(1,4,3,6, &a , &b);
-    printf("f(x)= %f x + %f",a,b);
     return 0;
 }
